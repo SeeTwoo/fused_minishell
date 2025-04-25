20dbfba816544c02902fbce0b12717677978ab0b
SeeTwoo <walti.bee@gmail.com>
2025-04-25 10:53:23 +0200

    changed header order for compiling

--- includes/minishell.h
+++ includes/minishell.h
@@ -20,9 +20,9 @@
 # include <dirent.h>
 # include <errno.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-# include <stdio.h>
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <termios.h>
